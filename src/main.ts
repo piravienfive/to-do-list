110ef505e72ed3ea2f2d22644cca8f7ae187e900
Thambi <thambi@Piraviens-MacBook-Pro.local>
2025-01-29 14:16:49 +0000

    added delete completed tasks button with functionality

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import './style.css'
 
+let deleteButton: any = ''
+
 function addTaskHandler (addBttn: HTMLButtonElement) {
+  let counter = 0
   console.log('clicked!')
   addBttn.style.display = 'none'
   let newInput = document.createElement('input')
   let newSaveButton = document.createElement('button')
   newSaveButton.innerText = 'Save'
+  // newSaveButton.disabled = true
   newSaveButton.style.fontSize = '30px'
   let listOfTasks = document.querySelector<HTMLDivElement>('.savedTasks')
   let addSection = document.querySelector<HTMLDivElement>('.addingTasks')
@@ -14,17 +18,49 @@ function addTaskHandler (addBttn: HTMLButtonElement) {
   }
   addSection.appendChild(newInput)
   addSection.appendChild(newSaveButton)
+  addSection.style.flexDirection = 'column'
+  addSection.style.justifyContent = 'center'
+  addSection.style.display = 'flex'
   newInput.style.fontSize = '30px'
+
+
+  // Another event listener need to be placed on input box, if input is present button enables
+  // if(newInput.value){
+  //   newSaveButton.disabled = false
+  // }
 
   newSaveButton.addEventListener('click' , () => {
     const divForTask = document.createElement('div')
+    counter += 1
+
+    
+
+    if(!deleteButton){
+      deleteButton = document.createElement('button')
+      const buttonsDiv = document.querySelector<HTMLDivElement>('.buttons')
+      buttonsDiv?.appendChild(deleteButton)
+      deleteButton.id = 'add'
+      deleteButton.innerText = 'Remove'
+    }
+    // deleteButton = document.createElement('button')
+    // const buttonsDiv = document.querySelector<HTMLDivElement>('.buttons')
+    // buttonsDiv?.appendChild(deleteButton)
+    // deleteButton.id = 'add'
+    // deleteButton.innerText = 'Remove'
     let task = newInput.value
     let newTaskCheckbox = document.createElement('input')
     newTaskCheckbox.type = 'checkbox'
-    newTaskCheckbox.id = 'task1'
+    newTaskCheckbox.id = 'tasks' + `${counter}`
+    newTaskCheckbox.className = 'tasks'
+
+    newTaskCheckbox.style.transform = 'scale(1.33)'
+    newTaskCheckbox.style.margin = '10px'
+
+
     let newInputLabel = document.createElement('label')
-    newInputLabel.setAttribute('for', 'task1')
+    newInputLabel.setAttribute('for', 'tasks' )
     newInputLabel.innerText = task
+    newInputLabel.id = 'tasks' + `${counter}`
     newInputLabel.style.fontFamily = 'Trebuchet MS'
     newInputLabel.style.fontSize = '23px'
 
@@ -39,6 +75,24 @@ function addTaskHandler (addBttn: HTMLButtonElement) {
     divForTask.appendChild(newTaskCheckbox)
     divForTask.appendChild(newInputLabel)
     // newTask.style.fontSize = '30px'
+
+
+    deleteButton.addEventListener('click', () => {
+      let currentTasks = document.querySelectorAll<HTMLInputElement>('.tasks')
+      console.log(currentTasks)
+      console.log(typeof(currentTasks))
+      for(let i = 0; i < currentTasks.length; i++){
+        console.log(currentTasks[i].checked)
+        if(currentTasks[i].checked == true){
+          currentTasks[i].remove()
+          document.querySelector<HTMLLabelElement>(`#tasks${i + 1}`)?.remove()
+
+        }
+      }
+
+    })
+
+
   })
 
   return 'hello'
